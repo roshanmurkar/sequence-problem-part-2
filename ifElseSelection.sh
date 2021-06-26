fff7def2cad012b35f8739de3705590b9741ec77
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-06-26 15:52:44 +0530

    Flip coin

--- ifElseSelection.sh
+++ ifElseSelection.sh
@@ -1,16 +1,12 @@
 #!/bin/bash -x
-#is leap year or not
 
-read -p  "Enter the year :-" y
+#flip coin
 
-con1=`expr $y % 4`
-con2=`expr $y % 100`
-con3=`expr $y % 400`
 
-if [ $con1 -eq 0 -a $con2 -ne 0 -o $con3 -eq 0 ]
+isHead=$((RANDOM%2))
+if [ $isHead -eq 0 ]
 then
-	echo $y " is a leap year "
+	echo "This is Head"
 else
-	echo $y " is not a leap year"
+	echo "this is Tail"
 fi
-
