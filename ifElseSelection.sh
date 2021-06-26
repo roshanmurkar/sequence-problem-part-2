1ecc4b299de74da772fdc41885dfa307b462d815
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-06-26 15:37:20 +0530

    date between 20-3 to 20-6

--- ifElseSelection.sh
+++ ifElseSelection.sh
@@ -1,49 +1,32 @@
 #!/bin/bash -x
 
-num1=$((RANDOM%1000));
-num2=$((RANDOM%1000));
-num3=$((RANDOM%1000));
-num4=$((RANDOM%1000));
-num5=$((RANDOM%1000));
+read -p "Enter the day :- " d
+read -p "enter the month :- " m
 
-if [ $num1 -gt $num2 ] && [ $num1 -gt $num3 ] && [ $num1 -gt $num4 ] && [ $num1 -gt $num5 ]
-then 
-	echo $num1 "is max"
-
-elif [ $num2 -gt $num3 ] && [ $num2 -gt $num4 ] && [ $num2 -gt $num5 ]
+if [ $m -gt 3 ] && [ $m -lt 6 ]
 then
-	echo $num2 "is max"
 
-elif [ $num3 -gt $num4 ] && [ $num3 -gt $num5 ]
+if [ $m -eq 3 ] && [ $d -gt 20 ] && [ $d -lt 32 ]
 then
-        echo $num3 "is max"
-
-elif [ $num4 -gt $num5 ]
-then
-        echo $num4 "is max"
-
-else
-	echo $num5 "is max"
+	echo "true"
 fi
-#to find minimum
-
-
-if [ $num1 -lt $num2 ] && [ $num1 -lt $num3 ] && [ $num1 -lt $num4 ] && [ $num1 -lt $num5 ]
+		
+if [ $m -eq 4 ] && [ $d -gt 0 ] && [ $d -lt 31 ]
 then
-        echo $num1 "is min"
-
-elif [ $num2 -lt $num3 ] && [ $num2 -lt $num4 ] && [ $num2 -lt $num5 ]
-then
-        echo $num2 "is min"
-
-elif [ $num3 -lt $num4 ] && [ $num3 -lt $num5 ]
-then
-        echo $num3 "is min"
-
-elif [ $num4 -lt $num5 ]
-then
-        echo $num4 "is min"
-
-else
-        echo $num5 "is min"
+	echo "true"
 fi
+
+if [ $m -eq 5 ] && [ $d -gt 0 ] && [ $d -lt 32 ]
+then
+        echo "true"
+fi
+
+if [ $m -eq 6 ] && [ $d -gt 0 ] && [ $d -lt 21 ]
+then
+	echo "true"
+fi
+else
+	echo "false"
+fi
+
+
