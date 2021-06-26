7b11d2d31e9b9d31248c205acee50543d5918ddf
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-06-26 17:32:50 +0530

    num to day

--- caseStatement.sh
+++ caseStatement.sh
@@ -1,2 +1,36 @@
-echo "welcome"
+#!/bin/bash -x
 
+isSun=1;
+isMon=2;
+isTue=3;
+isWed=4;
+isThu=5;
+isFri=6;
+isSat=7;
+
+read -p "Enter any value betwwn 1-7 :- " r
+
+case $r in
+	$isSun)
+		day=Sunday
+		;;
+        $isMon)
+                day=Monday
+                ;;
+        $isTue)
+                day=Tuesday
+                ;;
+        $isWed)
+                day=Wednesday
+                ;;
+        $isThu)
+                day=Thursday
+                ;;
+        $isFri)
+                day=Friday
+                ;;
+        $isSat)
+                day=Saturday
+                ;;
+esac
+echo $day
