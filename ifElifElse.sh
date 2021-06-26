d0988378bcada712cff46d0d9613eb3e7f4fb9e6
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-06-26 16:13:11 +0530

    day in a week

--- ifElifElse.sh
+++ ifElifElse.sh
@@ -1,2 +1,31 @@
-echo "welcome"
+#!/bin/bash -x
 
+read -p "Enter  number between 1-7 :-" n
+
+if [ $n -eq 1 ]
+then
+	echo " SUNDAY "
+
+elif [ $n -eq 2 ]
+then
+	echo "MONDAY"
+
+elif [ $n -eq 3 ]
+then
+        echo "TUESDAY"
+
+elif [ $n -eq 4 ]
+then
+        echo "WEDNESDAY"
+
+elif [ $n -eq 5 ]
+then
+        echo "THURSDAY"
+
+elif [ $n -eq 6 ]
+then
+        echo "FRIDAY"
+
+else
+	echo "SATURDAY"
+fi
