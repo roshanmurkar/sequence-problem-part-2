a76be10c79712ed14d01a1f195af8fe30313a296
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-06-26 16:37:57 +0530

    CR

--- ifElseSelection.sh
+++ ifElseSelection.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-<<<<<<< HEAD
+
 #flip coin
 
 
@@ -10,7 +10,7 @@ then
 	echo "This is Head"
 else
 	echo "this is Tail"
-=======
+
 read -p "Enter the day :- " d
 read -p "enter the month :- " m
 
@@ -21,7 +21,7 @@ if [ $m -eq 3 ] && [ $d -gt 20 ] && [ $d -lt 32 ]
 then
 	echo "true"
 fi
-		
+
 if [ $m -eq 4 ] && [ $d -gt 0 ] && [ $d -lt 31 ]
 then
 	echo "true"
@@ -38,7 +38,6 @@ then
 fi
 else
 	echo "false"
->>>>>>> Q2
 fi
 
 
