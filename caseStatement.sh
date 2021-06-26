6014684ced1c18191f34dfd0a0c381ff6f3e3331
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-06-26 17:25:59 +0530

    num into word using case

--- caseStatement.sh
+++ caseStatement.sh
@@ -1,2 +1,49 @@
-echo "welcome"
+#!/bin/bash -x
+
+isOne=1;
+isTwo=2;
+isThree=3;
+isFour=4;
+isFive=5;
+isSix=6;
+isSeven=7;
+isEight=8;isNine=9;
+isZero=0;
+
+read -p "enter number betwwn 0-9 :- " r
+
+case $r in
+	$isOne)
+		result=One
+		;;
+        $isTwo)
+                result=Two
+                ;;
+        $isThree)
+                result=Three
+                ;;
+        $isFour)
+                result=Four
+                ;;
+        $isFive)
+                result=Five
+                ;;
+        $isSix)
+                result=Six
+                ;;
+        $isSeven)
+                result=Seven
+                ;;
+        $isEight)
+                result=Eight
+                ;;
+        $isNine)
+                result=Nine
+                ;;
+        $isZero)
+                result=Zero
+                ;;
+esac
+echo $result
+
 
