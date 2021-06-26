88409422eda15d79d13ae31ba9eb6e107c78cadf
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-06-26 17:38:42 +0530

    read number

--- caseStatement.sh
+++ caseStatement.sh
@@ -1,2 +1,25 @@
-echo "welcome"
+#!/bin/bash -x
+
+is1=1;
+is10=10;
+is100=100;
+is1000=1000;
+
+read -p "enter a number from 1-10-100-1000 :-" r
+
+case $r in
+	$is1)
+		result=Unit
+		;;
+        $is1o)
+                result=Ten
+                ;;
+        $is100)
+                result=Hundred
+                ;;
+        $is1000)
+                result=Thousand
+                ;;
+esac
+echo $r " = " $result
 
